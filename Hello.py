02e5a32b33b2091d5c95e12f9c749854c9be61c2
Adityan Nainar <65527938+adityan-nainar@users.noreply.github.com>
2023-11-16 14:00:16 +0000

    done

--- Hello.py
+++ Hello.py
@@ -1,51 +1,97 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import plotly.express as px
+import pandas as pd
+import numpy as np
 
-LOGGER = get_logger(__name__)
+st.set_page_config(layout="wide")
 
+st.title('DA Project :smile:')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+df = pd.read_csv('/workspaces/da-project-testing/BillionairesStatisticsDataset.csv')
 
 
-if __name__ == "__main__":
-    run()
+#sidebar
+
+st.sidebar.header('Filters here: ')
+
+# country = st.sidebar.multiselect(
+#     "Select the country: ",
+#     options=df['country'].unique(),
+#     default=df['country'].iloc[1]   
+# )
+
+container = st.sidebar.container()
+all = st.sidebar.checkbox("Select all countries")
+ 
+if all:
+    country = container.multiselect("Select one or more options:",
+         df['country'].unique(),df['country'].unique())
+else:
+    country = container.multiselect("Select one or more options:",
+        df['country'].unique(),default=['India', 'Switzerland'])
+       
+# category = st.sidebar.multiselect(
+#     "Select the category: ",
+#     options=df['category'].unique(),
+#     default=df['category'].iloc[1]
+# )
+
+container2 = st.sidebar.container()
+all2 = st.sidebar.checkbox("Select all categories")
+ 
+if all2:
+    category = container2.multiselect("Select one or more options:",
+         df['category'].unique(),df['category'].unique())
+else:
+    category = container2.multiselect("Select one or more options:",
+        df['category'].unique(), default=['Technology'])
+  
+
+selfMade = st.sidebar.multiselect(
+    "Select selfMade : ",
+    options=df['selfMade'].unique(),
+    default=df['selfMade'].unique()
+)
+
+age = st.sidebar.slider(
+    'Select Age range',
+    0, 100, (25, 75)
+)
+
+df_selection = df.query(
+    "country == @country & category==@category & selfMade==@selfMade & age<=@age[1] & age>=@age[0]"
+)
+
+
+#Mainpage
+st.markdown("##")
+
+number_of_b = len(df_selection.index)
+avg_age = round(df_selection['age'].mean(),1)
+avg_final_worth = round(df_selection['finalWorth'].mean(),1)
+left_column, middle_column, right_column = st.columns(3)
+
+with left_column:
+    st.subheader("Number of billionaires: ")
+    st.subheader(f"{number_of_b}")
+with middle_column:
+    st.subheader("Average age: ")
+    st.subheader(f"{avg_age}")
+with right_column:
+    st.subheader("Average worth (in billion $): ")
+    st.subheader(f"{avg_final_worth}")
+
+st.markdown('---')
+
+
+figure = px.bar(df_selection, x = 'rank', y = 'finalWorth')
+figure.update_layout(title_text='Rank vs Worth', title_x=0.5)
+
+fig = px.pie(df_selection, values='finalWorth', names='country', title='Country wise percentage')
+fig.update_layout(title='Country wise percentage', title_x=0.37)
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(figure, use_container_width=True)
+right_column.plotly_chart(fig, use_container_width=True)
+
+st.dataframe(df_selection)
